c8a7783ffddbda21bb0f30c1770d78b2273d26ff
Magnus Sagerup <magnus.sagerup@gmail.com>
2017-04-26 18:58:19 +0200

    Add logger module

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 
 var chalk = require('chalk');
-var lodash = require('lodash');
+var _ = require('lodash');
+var logger = require('./lib/logger');
 
-
+// test chalk
 console.log(chalk.white.bgRed.bold('Error!!'));
 console.log(chalk.white.bgGreen.bold('It Works!'));
 
+// test lodash
 var myArray = ['Norway', 'Sweden', 'Denmark', 'Finland'];
-lodash.each(myArray, function(land){
+_.each(myArray, function(land){
     console.log(chalk.green.bold(land));
 });
+
+
+//logger
+
+logger.log('Message from info', 'info');
+logger.log('Message from warning', 'warning');
+logger.log('Message from Error', 'error');
